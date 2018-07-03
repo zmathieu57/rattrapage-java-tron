e66f8f56dc9cc2aec9ff6be0b35f7a7f3d7c8733
zmathieu57 <mathieu.zimmer@viacesi.fr>
2018-07-03 16:20:16 +0200

    dessin des lignes vue

--- tron/view/src/main/java/view/GameView.java
+++ tron/view/src/main/java/view/GameView.java
@@ -10,11 +10,10 @@ public class GameView extends JFrame{
 		game.setTitle("Tron : Game");
 		game.setSize(600,400);
 		game.setLocationRelativeTo(null);
+		game.setBackground(Color.darkGray);
 		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel panel2 = new JPanel();
-		panel2.setBackground(Color.darkGray);
-		game.setContentPane(panel2);
+		game.setContentPane(new DrawView());
 		
 		game.setVisible(true);
 		
